15cf92a16519e6e2feed131b839fdc61721b4f48
Momir Iliev <momir.iliev@gmail.com>
2023-10-04 21:48:21 +0200

    Sixth Homework

---
+++ 6. Sixth Homework/src/App.jsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { Todos } from "./components/Todos";
+import "./css/App.css";
+
+export function App() {
+  const [car, setCar] = useState({ model: "BMW", year: 2010 });
+  const [days, setDays] = useState(["Monday", "Tuesday", "Wednesday"]);
+  const [person, setPerson] = useState({ firstName: "", lastName: "" });
+  const [todos, setTodos] = useState([
+    // { id: 1, text: "Learn React", done: false },
+    // { id: 2, text: "Eat Dinner", done: false },
+    // { id: 3, text: "Go to Sleep", done: false },
+  ]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    console.log(person);
+    console.log(todos);
+  }, [person, todos]);
+
+  function newCar() {
+    setCar({
+      ...car,
+      model: "Yugo",
+      // year: 2020,
+    });
+  }
+  function addDays() {
+    setDays(["Sunday", ...days, "Thursday"]);
+  }
+
+  function addNewTodo() {
+    if (newTodo.trim() !== "") {
+      let newObject = {
+        id: randomId(),
+        text: newTodo,
+        done: false,
+      };
+      setTodos([...todos, newObject]);
+      setNewTodo("");
+    } else {
+      alert(
+        "No value has been entered\nPlease enter a value then submit the new Todo"
+      );
+    }
+  }
+  function markTodoAsDone(todo) {
+    console.log(todo);
+    setTodos([
+      //iterator -> item
+      ...todos.map(
+        (item) =>
+          item.id === todo.id //uslovot e najdi go kliknatiot element item.id (1,2,3) todo.id -> kliknatoto id na pr.2
+            ? { id: item.id, text: item.text, done: !item.done } //koga ke go najdeme kliknatiot element go menuvame samo propertito za done vo sprotivno koristejki negacija
+            : item //za site ostanati elementi ne gi cepkame odnosno go vrakjame orginalniot element nepromenet
+      ),
+    ]);
+  }
+
+  function randomId() {
+    let id = Math.floor(Math.random() * 1000);
+    console.log(id);
+    let uniqueId = todos.filter((todo) => todo.id === id);
+    if (uniqueId.length === 0) return id;
+  }
+
+  function deleteTodo(todo) {
+    setTodos([
+      ...todos.map((item) =>
+        item.id === todo.id ? { id: "", text: "", done: "" } : item
+      ),
+    ]);
+  }
+
+  return (
+    <div id="app">
+      <input
+        id="enterTodo"
+        type="text"
+        placeholder="Enter Todo"
+        value={newTodo}
+        onChange={(e) => {
+          setNewTodo(e.target.value);
+        }}
+      />
+      <button onClick={addNewTodo}>Add TODO </button>
+      <Todos
+        listOfTodosLength={todos.length}
+        listOfTodos={todos}
+        markTodoAsDone={markTodoAsDone}
+        deleteTodo={deleteTodo}
+      />
+      <div className="inputName">
+        <input
+          type="text"
+          placeholder="Enter First Name"
+          value={person.firstName}
+          onChange={(e) => {
+            setPerson({ ...person, firstName: e.target.value });
+          }}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter Last Name"
+          value={person.lastName}
+          onChange={(e) => {
+            setPerson({ ...person, lastName: e.target.value });
+          }}
+        />
+      </div>
+      <h2>Car:</h2>
+      <p>Model:{car.model} </p>
+      <p>Year: {car.year} </p>
+      <button onClick={newCar}>Buy new Car</button>
+      <hr />
+      <ul>
+        {days.map((day, i) => (
+          <li key={i}>{day}</li>
+        ))}
+      </ul>
+      <button onClick={addDays}>Add Days</button>
+    </div>
+  );
+}
---
+++ 6. Sixth Homework/src/components/Todos.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+export const Todos = ({ listOfTodos, markTodoAsDone, deleteTodo }) => {
+  {
+    if (listOfTodos.filter((todo) => todo.id !== "").length === 0) {
+      return <h3>There are not added item in the array yet!</h3>;
+    } else {
+      return (
+        <table border={1} className="todo-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Completed</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listOfTodos.map((todo, index) => {
+              console.log(todo);
+              if (todo.text !== "") {
+                return (
+                  <tr key={index}>
+                    <td>{todo.id}</td>
+                    <td>{todo.text}</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        value={todo.done}
+                        checked={todo.done}
+                        onChange={() => {
+                          markTodoAsDone(todo);
+                        }}
+                      />
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          deleteTodo(todo);
+                        }}
+                      >
+                        Delete
+                      </button>
+                      <button>Edit</button>
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
+      );
+    }
+  }
+};
+
+Todos.propTypes = {
+  listOfTodos: PropTypes.arrayOf(PropTypes.object),
+  markTodoAsDone: PropTypes.func,
+  deleteTodo: PropTypes.func,
+  todosLength: PropTypes.number,
+};
